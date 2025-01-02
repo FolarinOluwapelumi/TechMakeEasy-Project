9b211455a2f97dc42e239e6c6880db563fb3b904
Folarin <folarinoluwapelumi81@gmail.com>
2025-01-02 09:02:45 +0000

    ..

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ href, children, className = '' }) => {
   return (
     <a
       href={href}
-      className={`bg-purple-500 text-white px-6 py-2 font-bold hover:bg-purple-700 ${className}`}
+      className={`bg-purple-600 text-white px-6 py-2 font-bold hover:bg-purple-700 ${className}`}
     >
       {children}
     </a>
